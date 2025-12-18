12606dd3bb473627f4f3204452ad3d7c670301fd
1exmc <250421974+1exmc@users.noreply.github.com>
2025-12-18 22:22:50 +0100

    Initial commit

---
+++ src/client/java/me/oneex/thmtools/client/THMtoolsClient.java
@@ -0,0 +1,10 @@
+package me.oneex.thmtools.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class THMtoolsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/oneex/thmtools/THMtools.java
@@ -0,0 +1,10 @@
+package me.oneex.thmtools;
+
+import net.fabricmc.api.ModInitializer;
+
+public class THMtools implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
